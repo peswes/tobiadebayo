887bbbe51936f95641cf3746d70ca5b3da2c6c8e
Raphael Ilelaboye <65245334+Harkyzorithm@users.noreply.github.com>
2025-05-17 21:20:13 -0400

    newey

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -217,3 +217,27 @@
     });
 
 })(jQuery);
+// This is a custom script for the FAQs.
+
+  const questions = document.querySelectorAll(".faq-question");
+
+  questions.forEach((question) => {
+    question.addEventListener("click", () => {
+      const currentlyActive = document.querySelector(".faq-question.active");
+      if (currentlyActive && currentlyActive !== question) {
+        currentlyActive.classList.remove("active");
+        currentlyActive.nextElementSibling.style.maxHeight = null;
+      }
+
+      question.classList.toggle("active");
+      const answer = question.nextElementSibling;
+
+      if (question.classList.contains("active")) {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      } else {
+        answer.style.maxHeight = null;
+      }
+    });
+  });
+
+
